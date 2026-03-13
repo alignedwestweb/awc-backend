14f0a234f24f9abbe619163f222e6049cce6bf55
weston <weston@alignedwest.com>
2026-03-12 20:54:00 -0600

    created a new branch new-branch

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3001;
 
 const cors = require("cors");
 app.use(cors({ origin: ["http://localhost:5173", "https://alignedwest-chiropractic-web.vercel.app"] }));
@@ -11,4 +10,5 @@ app.use("/frames", express.static(path.join(__dirname, "frames")));
 
 app.get("/", (req, res) => res.send("Hello from backend!"));
 
-app.listen(PORT, () => console.log(`🚀 Server ready on port ${PORT}`));
+// Export for Vercel serverless functions
+module.exports = app;
